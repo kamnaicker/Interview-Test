77de3a32c060b5df50cd2770ed3b3ecb56bb1d8d
kamnaicker <85431471+kamnaicker@users.noreply.github.com>
2023-09-16 00:48:47 +0200

    Refactored Hero Class, to Implement IHero Interface.
    added logic to the evolve method to update list with new updated values.

--- InterviewTest/Controllers/Hero.cs
+++ InterviewTest/Controllers/Hero.cs
@@ -5,14 +5,22 @@ using System.Threading.Tasks;
 
 namespace InterviewTest.Controllers
 {
-    public class Hero
+    public class Hero:IHero
     {
         public string name { get; set; }
         public string power { get; set; }
         public List<KeyValuePair<string, int>> stats {get;set;}
         public void evolve(int statIncrease = 5)
         {
-            
+            int originalStat;
+            int newStat;
+
+            for(int i = 0; i < stats.Count; i++)
+            {
+                originalStat = stats[i].Value;
+                newStat = originalStat + originalStat/2 * statIncrease;
+                stats[i] = new KeyValuePair<string, int>(stats[i].Key, newStat);
+            }
         }
     }
 }
